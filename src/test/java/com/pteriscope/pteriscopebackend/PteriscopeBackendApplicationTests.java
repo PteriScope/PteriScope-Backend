80b54046bc2bb35d25a3ab28c066f977f6648d38
Edward240402 <eduard.coar@gmail.com>
2023-10-22 19:36:45 -0500

    first commit

---
+++ src/test/java/com/pteriscope/pteriscopebackend/PteriscopeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pteriscope.pteriscopebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PteriscopeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
